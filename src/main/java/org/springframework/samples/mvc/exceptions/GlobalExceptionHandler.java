4f694578698ad87a6a9fb749554d827b7f9416e5
QBNemo <15281107580@qq.com>
2017-07-18 19:55:29 +0800

    2017-07-18

--- src/main/java/org/springframework/samples/mvc/exceptions/GlobalExceptionHandler.java
+++ src/main/java/org/springframework/samples/mvc/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package org.springframework.samples.mvc.exceptions;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.act.ActUtil;
 import org.springframework.act.Mojo;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,14 +16,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+	private final Log logger = LogFactory.getLog(getClass());
+	
 	@ExceptionHandler
 	public @ResponseBody String handleBusinessException(BusinessException ex) {
+		logger.error("GlobalExceptionHandler handleBusinessException: " + ex.getMessage());
 		return "Handled BusinessException";
 	}
 	
 	@ModelAttribute("noUse")
 	// mojo参数 测试ModelFactory(List<InvocableHandlerMethod> invocableMethods, WebDataBinderFactory dataBinderFactory, SessionAttributesHandler sessionAttributesHandler)
 	NoUse testCAMA(@ModelAttribute("mojo") Mojo mojo) {
+		logger.error("GlobalExceptionHandler testCAMA: " + ActUtil.hashCode(mojo));
 		return new NoUse();
 	}
 	
@@ -30,6 +37,7 @@ public class GlobalExceptionHandler {
 	
 	@InitBinder
 	public void testCAIB(WebDataBinder binder) {
+		logger.error("GlobalExceptionHandler testCAIB: " + ActUtil.hashCode(binder));
 		binder.toString();
 	}
 }
